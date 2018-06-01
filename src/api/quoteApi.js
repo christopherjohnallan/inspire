2246eb9c461fe4653eb7f1f8cb9f781a8acfbf51
Chris Allan <christopher.john.allan@gmail.com>
2018-06-01 11:58:05 -0500

    Remove old code from quoteApi

--- src/api/quoteApi.js
+++ src/api/quoteApi.js
@@ -1,42 +1,16 @@
-/* const getQuote = () =>
-  fetch('http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json')
-    // response object has shape {quoteText, quoteAuthor}
-    .then(responseQuote => responseQuote.json())
-    .then(responseQuoteJson =>
-      (responseQuoteJson.quoteAuthor.length > 0
-        ? fetch(`https://en.wikipedia.org/w/api.php?&origin=*&action=query&generator=search&gsrnamespace=0&gsrlimit=1&prop=pageimages|extracts&pilimit=max&exintro&exlimit=max&continue&pithumbsize=100&gsrsearch=hastemplate%3ABirth_date+${encodeURIComponent(responseQuoteJson.quoteAuthor)}&format=json`)
-          .then(responseWiki => responseWiki.json())
-          .then((response) => {
-            const res = response.query.pages[Object.keys(response.query.pages)[0]];
-            return {
-              text: responseQuoteJson.quoteText,
-              author: {
-                name: responseQuoteJson.quoteAuthor,
-                imageUri: res.thumbnail.source,
-                url: '',
-                dateOfBirth: res.extract.substring(
-                  res.extract.indexOf('(') + 1,
-                  res.extract.indexOf('–') - 1,
-                ),
-                dateOfDeath: res.extract.substring(
-                  res.extract.indexOf('–') + 2,
-                  res.extract.indexOf(')'),
-                ),
-              },
-            };
-          })
-        : { text: responseQuoteJson.quoteText, author: {} })); */
-
 const getQuote = async () => {
   try {
     const responseQuote = await fetch('http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json');
     const quote = await responseQuote.json();
-    const responseWiki = await fetch(`https://en.wikipedia.org/w/api.php?&origin=*&action=query&generator=search&gsrnamespace=0&gsrlimit=1&prop=pageimages|extracts&pilimit=max&exintro&exlimit=max&continue&pithumbsize=100&gsrsearch=hastemplate%3ABirth_date+${encodeURIComponent(quote.quoteAuthor)}&format=json`);
-    const responseWikiJson = await responseWiki.json();
-    const author =
-      responseWikiJson.query.pages === undefined
-        ? null
-        : responseWikiJson.query.pages[Object.keys(responseWikiJson.query.pages)[0]];
+    let author = null;
+    if (quote.quoteAuthor.length !== 0) {
+      const responseWiki = await fetch(`https://en.wikipedia.org/w/api.php?&origin=*&action=query&generator=search&gsrnamespace=0&gsrlimit=1&prop=pageimages|extracts&pilimit=max&exintro&exlimit=max&continue&pithumbsize=100&gsrsearch=hastemplate%3ABirth_date+${encodeURIComponent(quote.quoteAuthor)}&format=json`);
+      const responseWikiJson = await responseWiki.json();
+      author =
+        responseWikiJson.query.pages === undefined
+          ? null
+          : responseWikiJson.query.pages[Object.keys(responseWikiJson.query.pages)[0]];
+    }
     return {
       text: quote.quoteText,
       author:
@@ -64,18 +38,3 @@ const getQuote = async () => {
 
 // eslint-disable-next-line import/prefer-default-export
 export { getQuote };
-
-/* https://en.wikipedia.org/w/api.php?&
-action=query
-generator=search
-gsrnamespace=0
-gsrlimit=20
-prop=pageimages|extracts
-pilimit=max
-exintro
-exsentences=1
-exlimit=max
-continue
-pithumbsize=100
-gsrsearch=hastemplate::Birth_date+Albert Einstein
-format=json */
